b639623c86193bb04c0d7ff0b736a44a2c47cdbb
AsamAdibul <147338853+AsamAdibul@users.noreply.github.com>
2024-10-06 17:27:39 +0600

    Create largestNum.js

---
+++ largestNum.js
@@ -0,0 +1,21 @@
+function largestNum(arr) {
+    let l = arr[0]; 
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > l) {
+            l = arr[i]; 
+        }
+    }
+    return l;
+}
+
+
+let size = parseInt(prompt("Enter the size of the array:"));
+let numbers = [];
+
+for (let i = 0; i < size; i++) {
+    let num = parseFloat(prompt());
+    numbers.push(num); 
+}
+
+let largestNumber = largestNum(numbers);
+console.log("The largest number is: " + largestNumber);
