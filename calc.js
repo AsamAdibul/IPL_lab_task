5330f4e80c11c52b8fce8e3247dd5a32d966528e
AsamAdibul <147338853+AsamAdibul@users.noreply.github.com>
2024-10-06 17:19:02 +0600

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,26 @@
+
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+
+
+
+document.write("Addition: " + add(num1, num2) + "\n");
+document.write("Subtraction: " + sub(num1, num2) + "\n");
+document.write("Multiplication: " + mul(num1, num2) + "\n");
+document.write("Division: " + div(num1, num2) + "\n");
